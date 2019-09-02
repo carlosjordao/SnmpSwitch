689fe95fcadb6bc3e96a90b5467c9ac45ef88bb2
Carlos <carlosjordao@gmail.com>
2019-09-02 00:36:35 -0300

    avoided clash of variable names

--- netstatus/lib/switch/switchlib.py
+++ netstatus/lib/switch/switchlib.py
@@ -2,16 +2,15 @@
     Static functions for Switch classes
 """
 import netsnmp
-import logging
 
 
 def format_mac(value):
     return value.replace('"', '').strip().replace(' ', ':').lower()
 
 
-def snmp_values(values, filter=False):
+def snmp_values(values, filter_=False):
     """ Clean the OID data from SNMP library """
-    if filter:
+    if filter_:
         return (v[netsnmp.VALUE].replace('"', '')
                 if v[netsnmp.TYPE] not in ('NOSUCHINSTANCE', 'NOSUCHOBJECT', 'NULL') else -1
                 for v in values
@@ -51,19 +50,12 @@ def lldp_is_uplink_extra(switch, lport, lldp_port):
             '.1.0.8802.1.1.2.1.5.32962.1.2.1.1.1.' + lport,
             )
     res = snmp_values(switch.sessao.get(oids))
-
-    # em vez de verificar quem é switch, vamos verificar quem certamente não é: APs.
-    # Isso é bem específico para nossas redes
-    # talvez seja bom deixar, no futuro, em algum arquivo de configuração
+    # checking specific variables for other type of devices.
     if res[2] == '20' or res[2] == '55':
         return False
-
     # return True if (res[2] == '1' or int(res[2]) >= 100) and (res[1] == '2' or res[0] == '1') else False
     # modificando: se POE está desligado OU power class == 1, então não será VOIP nem AP
     if res[1] == 2 or res[0] == '1':
         return True
-    logging.debug(('lldp_filter_is_uplink: poe: ', res[1]))
-    # return True if res[1] != '1' else False
-    # adicionar outros testes... o antigo está falho. Deixar como False até criar melhores condições
     return False
 
