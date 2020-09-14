c5a533219cdc380816ea597d60b6987e595a67a6
carlosjordao <carlosjordao@gmail.com>
2020-09-14 07:26:39 -0300

    * z

--- SnmpSwitch/settings.py
+++ SnmpSwitch/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+0)8@_s=mb)z-b3ffb0xr3p82lo2hzs6yr8o4et&hl_r_!sp48'
+SECRET_KEY = '111222333'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
